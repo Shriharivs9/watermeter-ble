81bdd0aee4101c7d8040b440b78d77acdaa5ac64
BinaryHarsh <binaryharsh292@gmail.com>
2025-01-02 14:35:35 +0530

    Update .gitignore to include .env files and modify App.tsx to conditionally render BLEInterface based on environment variable. Clean up code formatting for consistency.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BLEInterface from './ble';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BLEInterface from "./ble";
 
 export default function App() {
+  console.log("process.env.EXPO_BUILD >>", process.env.EXPO_PUBLIC_BUILD);
   return (
     <View style={styles.container}>
-<BLEInterface/>
+      {process.env.EXPO_PUBLIC_BUILD == "development" && <BLEInterface />}
+      <Text>Hello</Text>
     </View>
   );
 }
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
